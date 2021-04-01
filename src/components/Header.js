2cf2ee1d90640bec3607bb3fdd82e331ce2bffde
kihBootCamp <kimberleyheuer@hotmail.com>
2021-04-01 13:49:56 -0500

    init main

---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "../css/layout.css";
+
+function Header() {
+    return (
+        //   
+        <header className="App-background">
+            <nav>
+                <ul id="nav" className="nav">
+                    <li><a className="smoothscroll" href="#home">Home</a></li>
+                    <li><a className="smoothscroll" href="#about">About</a></li>
+                    <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+                    <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                    <li><a className="smoothscroll" href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+
+            <div className="row banner">
+                <div className="banner-text">
+                    <h1 className="responsive-headline">I am Kimberley Heuer.</h1>
+                    <h3 style={{ color: '#fff', fontFamily: 'sans-serif ' }}>devops developer science with tech
+                    </h3>
+
+                    <ul className="social">
+                   
+                         
+                            <li key="linkedin">
+                                <a href="https://www.linkedin.com/in/kimberley-heuer">
+                                    <i className="fa fa-linkedin"></i>
+                                </a>   
+                            </li>
+                            <li key="github">
+                            <a href="https://github.com/bootcampdev">
+                                <i className="fa fa-github"></i>
+                            </a>   
+                            </li>
+                            <li key="email">
+                            <a href="mailto:kimberleyheuer@hotmail.com">
+                                <i className="fa fa-envelope"></i>
+                            </a>   
+                        </li>
+                                                   
+
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
